63d7b739febf8d3545ec9a023cb66569b26a3e86
DontWorryIGotThis <kirankumar.reddy.18@live.com>
2015-06-12 13:17:48 -0400

    Fixed Files

--- Distributed Highly Available Calendar Tool for Workgroups/Client_Server/CS675-Project2/rmi_constants.java
+++
@@ -1,4 +0,0 @@
-public class rmi_constants {                                                   // Constants defining the Port number and the Calendar manager Reference
-	public static final int rmi_port = 8989;
-	public static final String rmi_CManager_Ref = "CManager";
-}
